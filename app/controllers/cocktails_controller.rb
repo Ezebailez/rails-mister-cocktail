c5ddc03352f14496f4d284a6b92cf97acd88e904
Ezequiel Bailez Altavista <ealtavista@gmail.com>
2021-02-18 17:06:36 +0100

    routes & cocktailsController

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,21 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'Your cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+end
